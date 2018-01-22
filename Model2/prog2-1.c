254b17a522de23f137acf97bfbe13f8e793fada5
naoki709mm <33851747+naoki709mm@users.noreply.github.com>
2018-01-22 17:45:20 +0900

    Add files via upload

--- Model2/prog2-1.c
+++ Model2/prog2-1.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-#define N 20
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#define N 30
 
 void print_queens(int Q[N][N],int n){ /*配列Qを探索し1だと●、0だと空白を表示する関数*/
   int x,y;
@@ -28,20 +32,90 @@ void make_W(double W[N][N][N][N],int n){
     for(j=0;j<n;j++){
       for(k=0;k<n;k++){
 	for(l=0;l<n;l++){
-	  W[i][j][k][l]=delta(i,k)*(1-delta(j,l))+delta(j,l)*(1-delta(i,k));
-	  W[i][j][k][l]+=delta(i+j,k+l)*(1-delta(i,k));
-	  W[i][j][k][l]+=delta(i-j,k-l)*(1-delta(i,k));
+	  W[i][j][k][l]=delta(i,k)*(1-delta(j,l))+delta(j,l)*(1-delta(i,k))+delta(i+j,k+l)*(1-delta(i,k))+delta(i-j,k-l)*(1-delta(i,k));
 	}
       }
     }
   }
 }
 
-/*doule energy(int x[N][N],double W[N][N][N][N],int n){
+double energy(int x[N][N],double W[N][N][N][N],int n){
   double E=0.0;
-*/
+  int i,j,k,l;
+  for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+      for(k=0;k<n;k++){
+	for(l=0;l<n;l++){
+	  E+=W[i][j][k][l]*x[i][j]*x[k][l];
+	}
+      }
+    }
+  }
+  return E;
+}
+
+void reset(double W[N][N][N][N],int x[N][N],int xd[N][N],int n){
+  int i,j,k,l;
+  for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+      for(k=0;k<n;k++){
+	for(l=0;l<n;l++){
+	  W[i][j][k][l]=0;
+	  x[i][j]=0;
+	  xd[i][j]=0;
+	}
+      }
+    }
+  }
+}
 
 int main(void){
+  int n,i,j,k,l;
+  int x[N][N],xd[N][N];
+  int P[N],r;
   double W[N][N][N][N]={0};
-  int x[N][N]
+  double Ec,En,dE;
+  time_t tp;
+  time(&tp);
+  srand48(tp);
+  printf("nを入力:");scanf("%d",&n);
+  reset(W,x,xd,n);
+  make_W(W,n);
+  for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+      if(i==j){
+	x[i][j]=1;
+	break;
+      }
+    }
+  }
+  Ec=energy(x,W,n);
+
+  for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+      if(x[i][j]==1){
+	P[i]=j;
+	break;
+      }
+    }
+  }
   
+  while(En==0){
+    for(i=0;i<n;i++){
+      for(j=0;j<n;j++){
+	xd[i][j]=x[i][j];
+      }
+    }
+    i=rand()%n;
+    k=rand()%n;
+    xd[i][P[i]]=0;
+    xd[i][P[k]]=1;
+    xd[k][P[k]]=0;
+    xs[k][P[i]]=1;
+    En=(xd,W,n);
+    dE=En-Ec;
+    if(dE<=0)
+ 
+  print_queens(x,n);
+  return 0;
+}
